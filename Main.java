fb2c4eff37fd4ea55f81c360e4b01773630504cf
XeeMpee <xeempee@gmail.com>
2018-07-13 12:35:51 +0200

    gama_correct

--- Main.java
+++ Main.java
@@ -16,7 +16,7 @@ public class Main {
             int tick = 0;
             int len = 8;
             int gamaMovement = -5;
-            for(int i=0;i<8; i++){
+            for(int i=0;i<16; i+=2){
                 ShortMessage mS = new ShortMessage();
                 mS.setMessage(144, 1, 'c'+i+(gamaMovement*7), 100);
                 MidiEvent nuteS = new MidiEvent(mS, tick);
@@ -27,13 +27,13 @@ public class Main {
                 track.add(nuteS);
                 track.add(nuteE);
             }
-            for(int i=0;i<8; i++){
+            for(int i=0;i<16; i+=2){
                 ShortMessage mS = new ShortMessage();
-                mS.setMessage(144, 1, 'c'+7-i+(gamaMovement*7), 100);
+                mS.setMessage(144, 1, 'c'+14-i+(gamaMovement*7), 100);
                 MidiEvent nuteS = new MidiEvent(mS, tick);
                 tick += len;
                 ShortMessage mE = new ShortMessage();
-                mE.setMessage(128, 1, 'c'+7-i+(gamaMovement*7), 100);
+                mE.setMessage(128, 1, 'c'+14-i+(gamaMovement*7), 100);
                 MidiEvent nuteE = new MidiEvent(mE, tick);
                 track.add(nuteS);
                 track.add(nuteE);
